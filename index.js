24d6b22ee2dcb51b1295ffb57c3bc189a69fa831
TendaniSadiki <61437061+TendaniSadiki@users.noreply.github.com>
2022-02-21 00:45:06 +0200

    {javascript(Project added)}

--- index.js
+++ index.js
@@ -1,4 +1,19 @@
-function Submit(){
-   
-    
-}
+
+
+//Myprojects
+let projects = ["How to calculate area with Python","How to calculate Range with python",
+"How to calculate average with python","How old will you be in 100 years Python",
+"How to convert hour to seconds using python","How to create a guess number game with python part 1"
+,"How to create a guess number game with python part 2","Which year will you be turning 100 years old with python"
+,"How to create Python Rock Paper Scissors (Beginner tutorial)"
+, "How to install python in visual studio code in windows 10 2021"]
+document.getElementById('project1').innerHTML = projects[0];
+document.getElementById('project2').innerHTML = projects[1];
+document.getElementById('project3').innerHTML = projects[2];
+document.getElementById('project4').innerHTML = projects[3];
+document.getElementById('project5').innerHTML = projects[4];
+document.getElementById('project6').innerHTML = projects[5];
+document.getElementById('project7').innerHTML = projects[6];
+document.getElementById('project8').innerHTML = projects[7];
+document.getElementById('project9').innerHTML = projects[8];
+document.getElementById('project10').innerHTML = projects[9];
